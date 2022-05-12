135576ef26995e6b16b8807a6aac1013275bf0ff
GreyRav <103762104+GreyRav@users.noreply.github.com>
2022-05-12 13:45:32 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import time
 import webbrowser
 
+validation = ["yes", "oui", "y"]
+
 #presentation
-print("Presentation du programme anti-ennui en 3 catégorie.\nLe premier sera les films.\nLe deuxieme, Les jeux vidéos.\nEt le troisieme, les reseaux sociaux (sociaux)")
+print("Presentation du programme anti-ennui en 3 catégorie.\nLe premier sera les films.\nLe deuxieme, Les jeux vidéos.\nEt le troisieme, les reseaux sociaux (sociaux)\n")
 
 #variable des categorie
 game = "Read Dead; " "Need For Speed; " "CS GO; " "Call of duty; " "The Witcher; " "Dark Soul; "
@@ -22,63 +24,47 @@ if choix == "game":
     print("Ce soir ce sera donc jeux vidéo, c'est partie...\n")
     time.sleep(2)
     print(game)
-    input("Appuie sur entrée pour continuer le programme. ;)")
+    input("Appuie sur entrée pour continuer le programme. ;)\n")
 
 elif choix == "Game":
     print("Ce soir ce sera donc jeux vidéo, c'est partie...\n")
     time.sleep(2)
     print(game)
-    input("Appuie sur entrée pour continuer le programme. ;)")
+    input("Appuie sur entrée pour continuer le programme. ;)\n")
 
 elif choix == "Reseaux":
     print("Ce soir ce sera donc les reseaux saociaux, c'est partie...\n")
     time.sleep(2)
     print(reseaux)
-    input("Appuie sur entrée pour continuer le programme. ;)")
+    input("Appuie sur entrée pour continuer le programme. ;)\n")
 
 elif choix == "reseaux":
     print("Ce soir ce sera donc les reseaux saociaux, c'est partie...\n")
     time.sleep(2)
     print(reseaux)
-    input("Appuie sur entrée pour continuer le programme. ;)")
+    input("Appuie sur entrée pour continuer le programme. ;)\n")
 
-elif choix == "film":
+elif choix == "film" or "Film":
     print("Ce soir ce sera donc un film, c'est partie...\n")
     time.sleep(2)
     print(film)
-    if input("Tu veux ouvrir netfix ? ") == "oui" or "y" or "ok":
+    if input("Tu veux ouvrir netfix ?\n\n") in validation:
         time.sleep(2)
-        webbrowser.open("https://www.netflix.com/browse")
-
-
+        webbrowser.open("https://www.netflix.com/browse\n")
     else:
-        input("Appuie sur entrée pour continuer le programme. ;)")
-
-elif choix == "Film":
-    print("Ce soir ce sera donc un film, c'est partie...\n")
-    time.sleep(2)
-    print(film)
-    if input("Tu veux ouvrir netfix ? ") == "oui" or "y" or "ok":
-        time.sleep(2)
-        webbrowser.open("https://www.netflix.com/browse")
-
-
-    else:
-        input("Appuie sur entrée pour continuer le programme. ;)")
-
+        input("Appuie sur entrée pour continuer le programme. ;)\n")
 else:
-    print("je n'ai pas compris, désolée.")
-    input("tape entrée pour quitter.")
+    print("je n'ai pas compris, désolée.\n")
     time.sleep(2)
 
-print("Pour toute demande de rajout de jeux, de film, ou de reseaux sociaux ou de nouvelle catégorie. N'hesité pas a m'ajouter sur discord: MisterRaven#5442\nEt si le code vous interesse, c'est sur mon github: https://github.com/GreyRav/programe-de-l-ennuie.git.\n")
-if input("Tu veux que j'ouvre github ?: ") == "oui" or "y" or "ok":
-    print("okay je te l'ouvre...")
+print("Pour toute demande de rajout de jeux, de film, ou de reseaux sociaux ou de nouvelle catégorie. N'hesité pas a m'ajouter sur discord: MisterRaven#5442\nEt si le code vous interesse, c'est sur mon github: https://github.com/GreyRav/programe-de-l-ennuie.\n")
+if input("Tu veux que j'ouvre github ?: \n") in validation:
+    print("okay je te l'ouvre...\n")
     time.sleep(2)
-    webbrowser.open("https://github.com/GreyRav/programe-de-l-ennuie.git")
+    webbrowser.open("https://github.com/GreyRav/programe-de-l-ennuie")
 else:
-    print("okay")
-
+    print("okay\n")
+print("\n")
 print("sur ce bonne soirée ;)\n")
 input("Appuie sur entrée pour quitter.")
 exit()
